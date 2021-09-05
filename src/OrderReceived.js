70911b46d0d38dce2eb53d2c92a8305429d1942b
Luis Lopez <llopez1518@gmail.com>
2021-09-05 11:03:44 -0600

    chagen order rec'd page

--- src/OrderReceived.js
+++ src/OrderReceived.js
@@ -1,35 +1,24 @@
-import React, { useState, useContext, useCallback, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import AppContext from "./AppContext";
+import items from "./MyOrder";
 
 export default function OrderReceived() {
   const [orderTime, setOrderTime] = useState(15);
-  const [orderItems, setOrderItems] = useState([]);
   const { order_id } = useContext(AppContext);
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/order_items/${order_id}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setOrderItems(data);
-      });
-  }, [order_id]);
-
-  return (
-    <>
-      <h2>Thank you for placing your order!</h2>
-
-      <h3>The total amount due at pick-up is: </h3>
-      <h4>
-        And your order will be ready in:{" "}
-        {orderItems.map((item, index) => {
+  /*
+  {" "}
+        {items.map((item, index) => {
           return (
             <>{item.quantity > 4 ? setOrderTime(orderTime + 10) : orderTime}</>
           );
         })}{" "}
-        minutes.
-      </h4>
+  */
+  return (
+    <>
+      <h2>Your order has been received!</h2>
+      <h3>Your order number is: {order_id} </h3>
+      <h4>Your order will be ready in: {orderTime} minutes.</h4>
     </>
   );
 }
